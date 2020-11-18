b1f18e58a8182c75dc65fbced46ee7f0b41fa09a
zhangboheng <zhangboheng827@gmail.com>
2020-11-18 17:51:48 +0800

    Add Pull-Push Bar Images

--- js/Javascript.js
+++ js/Javascript.js
@@ -14,6 +14,9 @@ $(document).ready(function () {
     $(".menu-top5").css({
         height: $(window).height() - 42
     });
+    $(".menu-top2").load("article/page1.html");
+    $(".menu-top3").load("book/page1.html");
+    $(".menu-top4").load("design/page1.html");
     $(".menu-top5").load("tool/page1.html");
     $(".mcode-list").css({
         height: $(window).height(),
